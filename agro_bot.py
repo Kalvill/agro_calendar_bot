4680c72d52051f946396e45f0ea23a128c2bcbe5
Kalvill <kalvill19@gmail.com>
2026-04-22 19:50:37 +0200

    BOT_TOKEN

--- agro_bot.py
+++ agro_bot.py
@@ -6,7 +6,7 @@ from telegram import Bot
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 # ========== НАЛАШТУВАННЯ ==========
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8641229015:AAHF6NmgYJM7ABHraR8MTib_FDXCWxESZuM")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8641229015:AAHmaBDmCZgf7-oHfoHtBfm2aZABnmH-M2g")
 CHAT_ID   = os.environ.get("CHAT_ID", "341010427")
 TZ        = pytz.timezone("Europe/Warsaw")
 # ==================================
